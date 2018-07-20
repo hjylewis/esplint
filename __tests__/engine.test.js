03cf614899e0001d82ef4bc715ef328bf133be60
Henry <hlewis@uchicago.edu>
2018-07-20 10:10:06 -0500

    Integration tests with basic happy paths

--- __tests__/engine.test.js
+++ __tests__/engine.test.js
@@ -2,6 +2,8 @@ const os = require("os");
 const path = require("path");
 const execa = require("execa");
 const log = require("../lib/log");
+const fs = require("fs");
+const stripJsonComments = require("strip-json-comments");
 
 log.log = jest.fn();
 log.error = jest.fn();
@@ -37,8 +39,8 @@ describe("engine", () => {
   });
 
   it("throws error if no esplint config", () => {
-    const path = getFixturePath("no-config");
-    process.chdir(path);
+    const fixturePath = getFixturePath("no-config");
+    process.chdir(fixturePath);
 
     const { run } = require("../lib/engine");
     expect(() => {
@@ -47,12 +49,71 @@ describe("engine", () => {
   });
 
   it("throws error if eslint error", () => {
-    const path = getFixturePath("eslint-error");
-    process.chdir(path);
+    const fixturePath = getFixturePath("eslint-error");
+    process.chdir(fixturePath);
 
     const { run } = require("../lib/engine");
     expect(() => {
       run({}, ["index.js"]);
     }).toThrowErrorMatchingSnapshot();
+  });
+
+  it("prints error if increase in eslint warnings", () => {
+    const fixturePath = getFixturePath("increase-warning");
+    process.chdir(fixturePath);
+
+    const { run } = require("../lib/engine");
+    const { results, hasError } = run({}, ["index.js"]);
+    expect(hasError).toEqual(true);
+    expect(results).toHaveLength(1);
+    expect(results[0].type).toEqual("error");
+    expect(results[0]).toMatchSnapshot();
+  });
+
+  it("no errors and changes record count when warnings decrease", () => {
+    const fixturePath = getFixturePath("decrease-warning");
+    process.chdir(fixturePath);
+
+    const { run } = require("../lib/engine");
+    const { results, hasError } = run({}, ["index.js"]);
+    expect(hasError).toEqual(false);
+    expect(results).toHaveLength(0);
+
+    const recordPath = path.join(fixturePath, ".esplint.rec.json");
+    const record = JSON.parse(
+      stripJsonComments(fs.readFileSync(recordPath, "utf8"))
+    );
+    expect(record.files["index.js"]["no-console"]).toEqual(1);
+
+    resetFixturePath("decrease-warning");
+  });
+
+  it("returns message about turning off warning when count is zero", () => {
+    const fixturePath = getFixturePath("decrease-warning-zero");
+    process.chdir(fixturePath);
+    const { run } = require("../lib/engine");
+
+    // Run twice
+    const [result1, result2] = [{}, {}].map(() => {
+      const { results, hasError } = run({}, ["index.js"]);
+      expect(hasError).toEqual(false);
+      expect(results).toHaveLength(1);
+      const result = results[0];
+      expect(result.type).toEqual("info");
+      return result;
+    });
+
+    // Get same result
+    expect(result1).toMatchObject(result2);
+    expect(result1).toMatchSnapshot();
+
+    // Count set to 0
+    const recordPath = path.join(fixturePath, ".esplint.rec.json");
+    const record = JSON.parse(
+      stripJsonComments(fs.readFileSync(recordPath, "utf8"))
+    );
+    expect(record.files["index.js"]["no-console"]).toEqual(0);
+
+    resetFixturePath("decrease-warning-zero");
   });
 });
