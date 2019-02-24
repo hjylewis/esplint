7deb233c1f9db96f6cbbf93a9c32c93d9cc59270
hjylewis <henry.jy.lewis@gmail.com>
2019-02-24 14:29:50 -0600

    updateFixtures script

---
+++ scripts/updateFixtures.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function updateFixtures() {
+  const fixturesDir = path.resolve("./__tests__/integration/fixtures/");
+  const fixtureNames = fs
+    .readdirSync(fixturesDir, {
+      withFileTypes: true
+    })
+    .filter(f => f.isDirectory())
+    .map(f => f.name);
+
+  fixtureNames.forEach(fixture => {
+    process.chdir(path.resolve(fixturesDir, `./${fixture}`));
+
+    // Make Changes here
+  });
+};
+
+module.exports();
