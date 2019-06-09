3448f242249fddb58b847753ddbe372c6ef89bb1
hjylewis <henry.jy.lewis@gmail.com>
2019-06-09 11:58:46 -0500

    Add suppress function to engine + tests

---
+++ __tests__/integration/engine/suppress.test.js
@@ -0,0 +1,109 @@
+const { fixtureInit } = require("../util");
+
+const { setup, readFile, onBeforeAll, onAfterEach, onAfterAll } = fixtureInit(
+  "engine.suppress"
+);
+
+describe("engine.suppress", () => {
+  beforeAll(onBeforeAll);
+
+  afterEach(onAfterEach);
+
+  afterAll(onAfterAll);
+
+  it(
+    "should insert a disable-eslint comment before each violation",
+    setup("increase-warning", ({ fixturePath }) => {
+      const { suppress } = require("../../../lib/engine");
+      suppress(["no-console"], ["."]);
+
+      const lines = readFile(fixturePath, "index.js")
+        .split("\n")
+        .filter(l => l.length > 0);
+      expect(lines).toEqual([
+        "// FIXME: The next line was auto suppressed by esplint",
+        "// eslint-disable-next-line no-console",
+        'console.log("");',
+        "// FIXME: The next line was auto suppressed by esplint",
+        "// eslint-disable-next-line no-console",
+        'console.log("");'
+      ]);
+    })
+  );
+
+  it(
+    "should insert disable-eslint comment for multiple files",
+    setup("multiple-files-no-record", ({ fixturePath }) => {
+      const { suppress } = require("../../../lib/engine");
+      suppress(["no-console"], ["."]);
+
+      ["index.js", "anotherFile.js"].forEach(filename => {
+        const lines = readFile(fixturePath, filename)
+          .split("\n")
+          .filter(l => l.length > 0);
+        expect(lines).toEqual([
+          "// FIXME: The next line was auto suppressed by esplint",
+          "// eslint-disable-next-line no-console",
+          'console.log("");',
+          "// FIXME: The next line was auto suppressed by esplint",
+          "// eslint-disable-next-line no-console",
+          'console.log("");'
+        ]);
+      });
+    })
+  );
+
+  it(
+    "should insert disable-eslint comment for multiple rules",
+    setup("extra-semi", ({ fixturePath }) => {
+      const { suppress } = require("../../../lib/engine");
+      suppress(["no-console", "no-extra-semi"], ["."]);
+
+      const lines = readFile(fixturePath, "index.js")
+        .split("\n")
+        .filter(l => l.length > 0);
+      expect(lines).toEqual([
+        "// FIXME: The next line was auto suppressed by esplint",
+        "// eslint-disable-next-line no-console, no-extra-semi",
+        'console.log("");;'
+      ]);
+    })
+  );
+
+  it(
+    "should preserve existing disable-eslint comments",
+    setup("suppressed-extra-semi", ({ fixturePath }) => {
+      const { suppress } = require("../../../lib/engine");
+      suppress(["no-console", "no-extra-semi"], ["."]);
+
+      const lines = readFile(fixturePath, "index.js")
+        .split("\n")
+        .filter(l => l.length > 0);
+      expect(lines).toEqual([
+        "// FIXME: The next line was auto suppressed by esplint",
+        "// eslint-disable-next-line no-console, no-extra-semi",
+        'console.log("");;'
+      ]);
+    })
+  );
+
+  it(
+    "should preserve leading whitespace",
+    setup("new-file-with-error", ({ fixturePath }) => {
+      const { suppress } = require("../../../lib/engine");
+      suppress(["no-console"], ["."]);
+
+      const lines = readFile(fixturePath, "newFile.js")
+        .split("\n")
+        .filter(l => l.length > 0);
+      expect(lines).toEqual([
+        "function newFile() {",
+        "  // FIXME: The next line was auto suppressed by esplint",
+        "  // eslint-disable-next-line no-console",
+        "  console.log();",
+        "}",
+        "newFile();"
+      ]);
+    })
+  );
+});
---
+++ __tests__/integration/fixtures/suppressed-extra-semi/index.js
@@ -0,0 +1,3 @@
+// FIXME: The next line was auto suppressed by esplint
+// eslint-disable-next-line no-console
+console.log("");
